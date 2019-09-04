ed3fcd13bea37b1368bb4adb495f381f18c06d42
Mike Holm <mholm@homebot.ai>
2019-09-03 22:30:15 -0600

    update education dates and projects

--- src/education.js
+++ src/education.js
@@ -28,10 +28,10 @@ Built several AI for competing against in Connect4 in Prolog. Built a map-reduce
     school: 'Madison High School',
     location: 'Madison, NJ',
     gpa: '3.75',
-    start: 'Sept 2007',
-    end: 'June 2011',
+    start: 'Sept 2003',
+    end: 'June 2007',
     description: `
-Recieved scholarship for performance in AP CS class. Built multiplayer networked battleship game in Visual Basic.
+Recieved scholarship for performance in AP CS class. Built multiplayer networked battleship game in Visual Basic. Built class-winning Mario tribute in Macromedia Flash detailing Mario's video game history. For each game system, you could sroll through games, when you selected one, the cartidge would insert into the console and the power light would illuminate. A screen would tell you about the game. There was a dungeon section where Boo would chase your mouse around the screen. The main menu had some flying Koopas and Mario would jump between pipes with your left and right arrow keys. When you landed on a pipe, a title would emerge from the pipe and hover above Mario's head. When you hit the down arrow, Mario would descend into the pipe and take you to that section of the presentation.
 `,
     tools: ['Java', 'HTML', 'CSS', 'Visual Basic', 'Windows']
   }
