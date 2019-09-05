e194b53c1b27f181b2f22a5505f67659d5535888
Mike Holm <mholm@homebot.ai>
2019-09-04 22:07:29 -0600

    add some stuff to the page

--- src/page.js
+++ src/page.js
@@ -1,16 +1,19 @@
 const H = require('jshtml')
 
-module.exports = ({
-  title,
-  content
-}) => (
-  H.html(
+module.exports = ({ title, content }) => `
+<!DOCTYPE html>
+  ${H.html(
+    { lang: 'en' },
     H.head(
-      H.meta({ name: 'viewport', content: 'width=device-width, initial-scale=1, minimum-scale=1, shrink-to-fit=no' }),
+      H.meta({
+        name: 'viewport',
+        content:
+          'width=device-width, initial-scale=1, minimum-scale=1, shrink-to-fit=no'
+      }),
       H.meta({ charset: 'utf-8' }),
       H.title(title),
       H.link({ rel: 'stylesheet', href: 'index.css' })
     ),
     H.body(content)
-  )
-)
+  )}
+`
